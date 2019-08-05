562b502c14bec865276c022a57cc9b4949c5887e
ykirichenko <k.yurets@gmail.com>
2019-08-05 17:46:59 +0300

    c

--- TokenController.cs
+++ TokenController.cs
@@ -99,20 +99,6 @@ namespace iknowscore.API.Controllers
             return Ok(new { token = tokenString });
         }
 
-        private string BuildToken()
-        {
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-            var token = new JwtSecurityToken(
-                _config["Jwt:Issuer"],
-                _config["Jwt:Issuer"],
-                expires: DateTime.Now.AddMinutes(30),
-                signingCredentials: creds);
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-
         public class LoginModel
         {
             public string Email { get; set; }
